c920b9dc17f412a1332f3c1fa4fcaf16a171a574
913434712kd <913434712@qq.com>
2018-10-29 22:28:24 +0800

    修改后

--- src/components/tabber/search.vue
+++ src/components/tabber/search.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="search">
-        <mt-search autofocus class="a"></mt-search>
+        <mt-search v-model="value" :result="filterResult"></mt-search>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            value:'',
+            list:['Apple','Banana','Orange','Durian','Lemon','Peach','Cherry',
+            'Berry','Core','Fig','Haw','Melon','Plum','Pear','Peanut','Other']
+        }
+    },
+    computed:{
+        filterResult() {
+            return this.list.filter(value => new RegExp(this.value, 'i').test(value));
+        }
+    }
 }
 </script>
 
